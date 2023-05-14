612fc917c6a77e5da61d76dfbdf1acfecc5aeb71
almirskaya <alexamirskaya@gmail.com>
2023-05-14 18:57:43 +0300

    deleted home work2
    deleted home work2

--- Homework2/task3.py
+++
@@ -1,9 +0,0 @@
-import re
-
-casino_blacklist = {"Kate Moss", "Liza Fin", "Liza Nope", "Richard Row"}
-poker_blacklist = {"Richard Row", "Liza Nope", "Ricky Martin"}
-alcohol_blacklist = {"Ricky Martin", "Kate Moss", "Richard Row", "Liza Nope"}
-
-names = str(casino_blacklist.intersection(poker_blacklist, alcohol_blacklist))
-names = re.sub("[{}']", "", names)
-print(f'Persons, who are on all blacklists: {names}')
--- Homework2/task6.py
+++
@@ -1,4 +0,0 @@
-non_unique = ['Monica Dow', 'Fibi Low', 'John Key', 'Monica Dow']
-unique_names = dict.fromkeys(non_unique)
-unique_names_list = list(unique_names.keys())
-print(f'Unique names: {unique_names_list}')
