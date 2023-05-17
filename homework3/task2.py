2f35c68bfe7ea2a62a1f3ea39f5e7dd2adbfd29f
Alexandra Mirskaya <alexandra.mirskaya@ab-soft.net>
2023-05-17 22:02:28 +0300

    hw3 task2 completed

---
+++ homework3/task2.py
@@ -0,0 +1,11 @@
+number = 2
+multiply_1 = number * 2
+multiply_2 = number + number
+multiply_3 = number << 1
+dividing_1 = number / 2
+dividing_2 = number // 2
+dividing_3 = number >> 1
+multiply_results = [multiply_1, multiply_2, multiply_3]
+dividing_results = [int(dividing_1), dividing_2, dividing_3]
+print(f'Multiply_results: {multiply_results}')
+print(f'Dividing_results: {dividing_results}')
