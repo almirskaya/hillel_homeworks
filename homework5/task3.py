4050ad982852324f9798ee8133d8460819d01ec7
almirskaya <alexamirskaya@gmail.com>
2023-05-25 17:45:04 +0300

    Update task3.py
    fix grammar mistake

--- homework5/task3.py
+++ homework5/task3.py
@@ -10,4 +10,4 @@ for letter in re.findall(r'([Aa-zZ])', all_text.lower(), flags=0):
         letter_counter[letter] = 1
 sorted_counter = sorted(letter_counter.items(), key=lambda x: (x[0], x[1]))
 for letter, count in sorted_counter:
-    print(f'The letter "{letter}" appears "{count}" times in out file ')
+    print(f'The letter "{letter}" appears "{count}" times in our file ')
